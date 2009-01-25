17dea91f5452982b401290c74bcf808c430f3894
rudle <seansorrell@gmail.com>
2009-01-24 20:31:23 -0500

    Some unknown stuff was not added?

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,23 @@
+class UsersController < ApplicationController
+  # render new.rhtml
+  def new
+    @user = User.new
+    respond_to do |format|
+      format.html # new.html.erb
+    end
+  end
+ 
+  def create
+    logout_keeping_session!
+    @user = User.new(params[:user])
+    success = @user && @user.save
+    if success && @user.errors.empty?
+      self.current_user = @user # !! now logged in
+      redirect_back_or_default('/')
+      flash[:notice] = "Thanks for signing up! You are now logged in as #{@user.to_s}" 
+    else
+      flash[:error]  = "We couldn't set up that account, sorry.  Please try again, or contact an admin (link is above)."
+      render :action => 'new'
+    end
+  end
+end
