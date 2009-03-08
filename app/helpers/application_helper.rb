e4cd54f4cbc328d5f7c65db2913b366b71b6243b
rudle <seansorrell@gmail.com>
2009-03-08 14:53:31 -0400

    Cleaned up some forms

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,7 +1,11 @@
 # Methods added to this helper will be available to all templates in the application.
 module ApplicationHelper
   def tab_link_to(name, options={}, html_options={}) 
-    html_options.merge!({ :id => 'current' }) if current_page?(options)
+    html_options.merge!({ :id => 'current' }) if current_controller?(name.downcase)
     link_to name, options, html_options 
+  end
+
+  def current_controller?(c)
+    controller.controller_name == c
   end
 end
